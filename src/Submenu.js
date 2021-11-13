d1b9c49c936edfb969e2f23fbdfbe2ddfc7fe2fd
Oyewo Bolaji Sodiq <oyewob@gmail.com>
2021-11-13 20:52:19 +0100

    first commit

---
+++ src/Submenu.js
@@ -0,0 +1,47 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+import {useGlobalContext} from './context';
+
+const Submenu = () => {
+  const {isSubMenuOpen, location, page: {page, links}} = useGlobalContext();
+  const container = useRef(null);
+  const [column, setColumn] = useState('col-2');
+
+  useEffect(() => {
+    setColumn('col-2');
+
+    const submenu = container.current;
+    const {center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+
+    if(links.length === 3) {
+      setColumn('col-3');
+    } 
+    if (links.length > 3) {
+      setColumn('col-4')
+    }
+  }, [location, links])
+
+  return (
+    <aside 
+    className={`${isSubMenuOpen ? 'submenu show' : 'submenu'}`} 
+    ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${column}`}>
+        {links.map((link, index) => {
+          const {label, icon, url } = link;
+          return (
+            <a  key={index} href={url} >
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
+    </aside>
+  )
+}
+
+export default Submenu
---
+++ src/context.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from 'react'
+import sublinks from './data'
+
+export const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+    const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({page: "", links: []})
+
+    const openSideBar = () => setIsSideBarOpen(true);
+    const closeSideBar = () => setIsSideBarOpen(false);
+
+    const openSubMenu = (text, coordinates) => {
+        const page = sublinks.find(sublink => sublink.page === text);
+        setPage(page);
+        setLocation(coordinates)
+        setIsSubMenuOpen(true);
+    }
+    const closeSubMenu = () => setIsSubMenuOpen(false);
+    return (
+        <AppContext.Provider value={{
+            isSideBarOpen,
+            isSubMenuOpen,
+            openSideBar,
+            closeSideBar,
+            openSubMenu,
+            closeSubMenu,
+            location,
+            page
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
